85f28da5f7cf35634da777e4031d13497c51c5bf
Андрей Терещенко <a.tereshchenko@infosec.ru>
2024-02-15 15:46:19 +0000

    PRTL-28 init empty front project

---
+++ src/global.d.ts
@@ -0,0 +1,12 @@
+declare module '*.png';
+declare module '*.jpg';
+
+declare module '*.svg' {
+  import React from 'react';
+
+  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement> & { title?: string }>;
+
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+
+  export default ReactComponent;
+}
